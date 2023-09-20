45e4190fa4aa321d8728c9298960099d8faa91f9
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-20 01:53:17 +0100

    City and User group updated

--- models/user.py
+++ models/user.py
@@ -18,3 +18,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship(
+        "Place", cascade="all, delete, delete-orphan", backref="user")
