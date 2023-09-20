ffaf7fc6e40cc2e16e93939951ef459192672a6a
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-20 10:26:35 +0100

    updates made on getter property in state model2

--- models/state.py
+++ models/state.py
@@ -10,16 +10,22 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship(
-        "City", cascade="all, delete, delete-orphan", backref="state")
 
-    @property
-    def cities(self):
-        """getter attribute cities that returns the list of City instances"""
-        from models import storage
-        from models.city import City
-        cities_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                cities_list.append(city)
-        return cities_list
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship(
+            "City",
+            backref="state",
+            cascade="all, delete, delete-orphan",
+            backref="state")
+    else:
+        @property
+        def cities(self):
+            """getter attribute cities that returns
+            the list of City instances"""
+            from models import storage
+            from models.city import City
+            cities_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
